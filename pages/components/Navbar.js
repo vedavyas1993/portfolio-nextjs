af61fa86a11909ad361ce60d005701e263a85425
vedavyas1993 <vedavyas1993@gmail.com>
2023-01-15 00:02:16 +0530

    added side menu close on backdrop click

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -67,56 +67,59 @@ function Navbar() {
         className={
           nav ? "fixed left-0 top-0 md:hidden w-full h-screen bg-black/70" : ""
         }
+        onClick={(e) => {
+          e.stopPropagation();
+          handleNav();
+        }}
+      ></div>
+      <div
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[85%] sm:w-[60%] md:w-[50%] h-screen bg-[#ecf0fe] p-10 ease-in duration-500 overflow-auto"
+            : "fixed left-[-200%] w-[85%] sm:w-[60%] md:w-[50%] h-screen bg-[#ecf0fe] top-0 p-10 ease-in duration-500 overflow-auto"
+        }
       >
-        <div
-          className={
-            nav
-              ? "fixed left-0 top-0 w-[85%] sm:w-[60%] md:w-[50%] h-screen bg-[#ecf0fe] p-10 ease-in duration-500 overflow-auto"
-              : "fixed left-[-200%] w-[85%] sm:w-[60%] md:w-[50%] h-screen bg-[#ecf0fe] top-0 p-10 ease-in duration-500 overflow-auto"
-          }
-        >
-          <div>
-            <div className="flex w-full items-center justify-between">
-              <Image
-                src="/images/logos/shortLogo.png"
-                alt="logo"
-                width="70"
-                height="30"
-              />
-              <div
-                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
-                onClick={handleNav}
-              >
-                <AiOutlineClose size={25} />
-              </div>
-            </div>
-            <div className="border-b border-gray-300 my-4">
-              <p className="w-[85%] md:w-[90%] py-4">
-                Let's build something legendary together
-              </p>
+        <div>
+          <div className="flex w-full items-center justify-between">
+            <Image
+              src="/images/logos/shortLogo.png"
+              alt="logo"
+              width="70"
+              height="30"
+            />
+            <div
+              className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              onClick={handleNav}
+            >
+              <AiOutlineClose size={25} />
             </div>
           </div>
-          <div className="py-4 flex flex-col">
-            <ul className="uppercase">
-              {navList.map((navItem) => {
-                return (
-                  <Link href={navItem.path}>
-                    <li className="py-4 text-sm">{navItem.title}</li>
-                  </Link>
-                );
-              })}
-            </ul>
-            <div className="pt-40">
-              <p className="uppercase tracking-widest text-[#5651e5]">
-                Lets's connect
-              </p>
-              <div className="flex items-center justify-between my-4 sm:w-[80%] md:w-[70%] lg:w-[30%] flex-wrap gap-1">
-                {socialLinks.map((social) => (
-                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
-                    {social.icon}
-                  </div>
-                ))}
-              </div>
+          <div className="border-b border-gray-300 my-4">
+            <p className="w-[85%] md:w-[90%] py-4">
+              Let's build something legendary together
+            </p>
+          </div>
+        </div>
+        <div className="py-4 flex flex-col">
+          <ul className="uppercase">
+            {navList.map((navItem) => {
+              return (
+                <Link href={navItem.path}>
+                  <li className="py-4 text-sm">{navItem.title}</li>
+                </Link>
+              );
+            })}
+          </ul>
+          <div className="pt-40">
+            <p className="uppercase tracking-widest text-[#5651e5]">
+              Lets's connect
+            </p>
+            <div className="flex items-center justify-between my-4 sm:w-[80%] md:w-[70%] lg:w-[30%] flex-wrap gap-1">
+              {socialLinks.map((social) => (
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  {social.icon}
+                </div>
+              ))}
             </div>
           </div>
         </div>
